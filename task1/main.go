9ce863a1a21c215afd30fd88f31e85c6c554c03a
Evgeniy Trunov <anyway.trunov@gmail.com>
2023-03-29 19:10:45 +0300

    commit2

--- task1/main.go
+++ task1/main.go
@@ -3,5 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("sdsd")
+	for i:=1; i <= 10; i++ {
+		for j:=1; j <=10; j++ {
+			fmt.Println(i, "*", j , "=", i*j) 
+		}
+		fmt.Println("")
+	} 
+	
 }
