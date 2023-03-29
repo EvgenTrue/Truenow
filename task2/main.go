34b2998d7e13b4863016fa887b1bc5c843ab6b5d
Evgeniy Trunov <anyway.trunov@gmail.com>
2023-03-29 20:59:41 +0300

    5

--- task2/main.go
+++ task2/main.go
@@ -3,12 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	var a, c int
+	var a  int
 	fmt.Scanln(&a)
 	for i:=1; i <= a; i++ {
-		if c = i%3 || i%5
-			fmt.Println(c) 
+		if  i%3 ==0 || i%5 == 0{
+			fmt.Println(i)
 		}
 	}
-	
+}
 	
