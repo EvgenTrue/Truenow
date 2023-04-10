02b928309a06ed05a07ea1551409051a3dde65e9
Evgeniy Trunov <anyway.trunov@gmail.com>
2023-04-10 22:48:29 +0300

    task6

--- task6/main.go
+++ task6/main.go
@@ -3,24 +3,11 @@ package main
 import "fmt"
  
 func main() {
-    var number, i, j int
-    var flag bool
- 
-    fmt.Print("Введите число для вывода всех простых чисел до данного числа: ")
-    
-    fmt.Scan(&number)
-    for i = 2; i < number; i++ {
-        
-        flag = false
-        for j = 2; j <= i /2; j++ {
-            
-            if i  %j == 0 {
-                flag = true
-            }
-        }
-        if flag == false {
-            fmt.Println(i)
-        }
-    }
- 
+var a int
+var b int
+fmt.Scan(&a)
+for i:=1; i<=a; i++{
+  if b%1==0 && b%b==0
+  fmt.Println("i, (i+1)") 
+}
 }
