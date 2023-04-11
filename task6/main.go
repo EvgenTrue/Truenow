93dea2727167a88ff2738a83108602aac69ebed0
Evgeniy Trunov <anyway.trunov@gmail.com>
2023-04-11 22:04:30 +0300

    commit8

--- task6/main.go
+++ task6/main.go
@@ -1,13 +1,25 @@
 package main
- 
+
 import "fmt"
- 
+
 func main() {
-var a int
-var b int
-fmt.Scan(&a)
-for i:=1; i<=a; i++{
-  if b%1==0 && b%b==0
-  fmt.Println("i, (i+1)") 
+	var a int
+
+	fmt.Scan(&a)
+	for i := 4; i <= a; i++ {
+		for j := 2; j <= i; j++ {
+			if i%j == 0 && i != j {
+				break
+
+			}
+			fmt.Println(i)
+		}
+
+	}
 }
-}
+
+// i j a=7
+// i=4  j=2  i%j =0 ==0 true
+
+// i=5 j=2   i%J !=0 false
+// i=5 j=3   i%j  !=0 false
--- task7/main.go
+++ task7/main.go
@@ -1,12 +1,12 @@
 package main
-import 
-"fmt"
 
+import "fmt"
 
-func main(){
-	fmt.Print("vvedi slovo:")
-	fmt.Scan("dog")
-	 
-	
-	fmt.Println ("god")
+func main() {
+	a := "hello"
+	for i:=len(a)- 1 ; i>=0; i--{
+		b:=a[i]
+//	for i,b:= range a {
+			fmt.Println(i,string(b))
+	}
 }
