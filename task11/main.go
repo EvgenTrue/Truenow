db7afa2892c550e56827e133c8f3d9c1755a934f
Evgeniy Trunov <anyway.trunov@gmail.com>
2023-04-16 15:39:21 +0300

    commit 11

---
+++ task11/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	
+	a := 9
+	for i := 1; i <= a; i++ {
+		for j := 1; j <= i; j++ {
+			return	
+			fmt.Print("")
+
+		}
+		
+			fmt.Println("*")
+
+		
+
+	}
+}
